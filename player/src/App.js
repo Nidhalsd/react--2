b733b1a35dfbae13ad8bfa3ac278e6ab16c612d8
Nidhalsd <nidhalessaidi@gmail.com>
2024-08-25 01:31:37 +0100

    first commit

---
+++ player/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PlayersList from './PlayersList';
+
+const App = () => {
+  return (
+    <div className="App">
+      
+      <h1 style={{ textAlign: 'center' }}> Best Player Cards</h1>
+      <PlayersList />
+    </div>
+  );
+};
+
+export default App;
---
+++ player/src/players.js
@@ -0,0 +1,36 @@
+const players = [
+    {
+           name: "Ronaldo" ,
+           team: "Al-nassr" ,
+           nationality: "Portougaise", 
+           jerseyNumber: 7 ,
+           age : 39 ,
+           imageUrl : "https://tmssl.akamaized.net/images/foto/galerie/cristiano-ronaldo-al-nassr-2023-1690844161-112905.jpg?lm=1690844177"
+    }, 
+    {
+            name: "Kylian Mbappé",
+            team: "Real Madrid",
+            nationality: "French",
+            jerseyNumber: 7,
+            age: 25,
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoEA8AqsezNJmJ3C9fyrLsLW2TVjs4M1nCTA&shttps://publish-p47754-e237306.adobeaemcloud.com/adobe/dynamicmedia/deliver/dm-aid--5caac151-8ab7-4320-80f4-0328f9b4e743/ND_PERFIL_NUEVA_1PC9260.app.png?preferwebp=true"
+    }, 
+    {
+            name: "Lionel Messi",
+            team: "Inter Miami CF",
+            nationality: "Argentinian",
+            jerseyNumber: 10,
+            age: 36,
+            imageUrl: "https://cdn.britannica.com/35/238335-050-2CB2EB8A/Lionel-Messi-Argentina-Netherlands-World-Cup-Qatar-2022.jpg"
+    },
+    {
+        name: "Erling Haaland",
+        team: "Manchester City",
+        nationality: "Norwegian",
+        jerseyNumber: 9,
+        age: 24,
+        imageUrl: "https://www.fifpro.org/media/1i0cxvm4/haaland_ucl.jpg"
+    }
+    ]
+    
+    export default players
